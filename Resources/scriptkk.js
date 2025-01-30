d4dfcb4d76a73ca724fb9e1474c0fbe662c98bc0
Martinn-art <martn0@centrum.cz>
2025-01-30 20:58:48 +0100

    js

---
+++ Resources/scriptkk.js
@@ -0,0 +1,27 @@
+
+
+
+btn.addEventListener ('click', () => { nadpis.style.display () = 'none')
+} ;
+ 
+InputDeviceInfo.addEventListener('keyup', (e) => {
+
+    lue
+
+    
+
+})
+    if(value.length > 8){
+
+       // document.getElementById('error-message')
+
+      //  console.log('délka je větší než 8');
+
+      //console.log(document.getElementById('error-message').style.display)
+
+      document.getElementById ('error-message').style.display = "block" ;
+    }else{
+        document.getElementById('error-message').style.display = "none" ;   }
+
+
+    }
---
+++ script.js
@@ -0,0 +1,14 @@
+switch (p) {
+    case 1:
+        console.log('pondělí');
+        
+        break;
+
+    case 2:
+        console.log('úterý')
+        break;
+    
+    case 3:
+        console.log('Jiný den')
+}
+
