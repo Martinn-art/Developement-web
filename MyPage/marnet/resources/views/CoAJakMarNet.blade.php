babe7344a23efc7dea8a83dae7bb1a2990be7148
Martinn-art <martn0@centrum.cz>
2025-05-13 23:12:21 +0200

    com

---
+++ MyPage/marnet/resources/views/CoAJakMarNet.blade.php
@@ -0,0 +1,22 @@
+<x-BackgroundMarNet>
+<x-NavMarNet>
+
+</x-NavMarNet>
+
+<div class="place-items-center justify-center text-balance w-min">
+<h1 class="text-white">Laravel je framework ve které vzniká moje práce</h1>
+<hr>
+<h4 class="text-white">Dále využivám TailWind, který si můžete představit jako již napsané CSS</h4>
+<br>
+<h4 class="text-white">Poté používám Alpine.js, představte si Javascript zjednodušeně</h4>
+<br>
+<h4 class="text-white">Kromě výše uvedeného, mi není cizí ani práce z databázemi.</h4>
+</div>
+
+
+
+
+
+
+
+</x-BackgroundMarNet>
---
+++ MyPage/marnet/resources/views/UvodMarNet.blade.php
@@ -0,0 +1,26 @@
+<x-BackgroundMarNet>
+    <x-NavMarNet>
+
+    </x-NavMarNet>
+
+    <div class="place-items-center justify-center" >
+    <h1 class=" text-white">Vítám tě na svém webu</h1>
+    <br>
+    <br>
+    <h4 class=" text-white text-wrap text-center lg:w-xl">Vytvářím webový obsah pro všechny, kdo o to stojí a pro toho kdo potřebuje být vidět.</h4>
+    <hr>
+
+
+    <h4 class=" text-white text-center"><span class=""> Moje práce začíná osobní schůzkou/online schůzkou a pokračuje dalšími schůzkami s již konkrétním návrhem řešení.</span></h4>
+    <br>
+    <br>
+    <h4 class="text-white text-center"><span class=""> Samozřejmě nejvíc pomůže, když někdo svoji představu má a nějak ji dokáže zhmotnit (pomůžou fotky, nákres webu, konkrétní představa).</span></h4>
+    </div>
+<br>
+<div class="lg:w-1/2 lg:h-1/2 lg:flex">
+   <img class="" src="{{ asset('PIC/CZ.png') }}" alt="Certifikat">
+<br>
+   <img class="" src="{{ asset('PIC/ENG.png') }}" alt="Certifikat">
+</div>
+
+    </x-BackgroundMarNet>
